f259d349ac9f1fb38e31493a6bcb15a37ba618f8
dynSR <dylan.schumacker+github@outlook.fr>
2026-06-25 13:12:22 +0200

    Corrected GetTotalFramesCount function output

--- Extensions/AnimationClipExtensions.cs
+++ Extensions/AnimationClipExtensions.cs
@@ -5,6 +5,6 @@ namespace Utilitas
     public static class AnimationClipExtensions
     {
         public static int GetTotalFramesCount(this AnimationClip animationClip) =>
-            Mathf.RoundToInt(animationClip.length * animationClip.frameRate);
+            animationClip == null ? 0 : Mathf.RoundToInt(animationClip.length * animationClip.frameRate);
     }
 }
